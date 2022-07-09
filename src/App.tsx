fb70d227cee8519f3b4237fbac473405a9449a5e
Efsane <efs.gun@gmail.com>
2022-07-09 20:03:25 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Toaster } from "react-hot-toast";
 
 import TodoApp from "./views/TodoApp/todo";
 import Home from "./views/Home/home";
@@ -8,6 +9,7 @@ import Home from "./views/Home/home";
 const App: FC = () => {
   return (
     <ChakraProvider>
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<TodoApp />} />
